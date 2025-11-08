36a70d57a0a8c21ab9a81127d482c556cd840d73
seven.it.work <seven.it.work@gmail.com>
2025-11-08 16:24:47 +0800

    feat(修仙者): 添加队伍系统并完善基础属性设置器
    添加修仙者队伍类，支持3x3成员管理及变化事件通知
    为BaseCultivator的基础属性添加setter/getter方法

--- godot-compoent/战斗/entity/修仙者.gd
+++ godot-compoent/战斗/entity/修仙者.gd
@@ -1,20 +1,48 @@
 # 修仙者类 - 游戏中的修仙者角色表示
 class_name Cultivator
 
+## 修仙者队伍类
+class 修仙者队伍:
+	## 队伍成员 3*3 的数组
+	var _members:Array[Array]=[
+		[],
+		[],
+		[],
+	]:set=set_members,get=get_members
+	signal 队伍成员变化(行:int,列:int,修仙者:Cultivator)
+	
+	## 设置队伍成员数组
+	func set_members(new_members:Array[Array]) -> void:
+		_members = new_members
+	
+	## 获取队伍成员数组
+	func get_members() -> Array[Array]:
+		return _members
+	
+	#region 外部方法
+	## 上阵
+	func 上阵(修仙者:Cultivator,行:int,列:int) -> void:
+		_members[行][列]=修仙者
+		队伍成员变化.emit(行,列,修仙者)
+	func 出队(行:int,列:int) -> void:
+		_members[行][列]=null
+		队伍成员变化.emit(行,列,null)
+	#endregion
+
 # 基础修仙者对象类 - 修仙者角色属性的核心数据结构
 class BaseCultivator:
 	# 修仙者姓名
-	var _name:String
+	var _name:String="基础修仙者":set=set_name,get=get_name
 	# 修仙者等级（使用GrowthValue类型进行渐进式成长）
-	var _level:BaseValue.GrowthValue=BaseValue.GrowthValue.new({"value":1,"min_growth":1,"max_growth":1,"growth_factor":1})
+	var _level:BaseValue.GrowthValue=BaseValue.GrowthValue.new({"value":1,"min_growth":1,"max_growth":1,"growth_factor":1}):set=set_level,get=get_level
 	# 修仙者生命值（使用RangeGrowth跟踪最小值/最大值范围）
-	var _health:BaseValue.RangeGrowth=BaseValue.RangeGrowth.new({"value":100,"max":100,"min":0,"min_growth":3,"max_growth":7,"growth_factor":5,"should_only_grow_max_value":true})
+	var _health:BaseValue.RangeGrowth=BaseValue.RangeGrowth.new({"value":100,"max":100,"min":0,"min_growth":3,"max_growth":7,"growth_factor":5,"should_only_grow_max_value":true}):set=set_health,get=get_health
 	# 修仙者灵气值（使用RangeGrowth跟踪最小值/最大值范围）
-	var _mana:BaseValue.RangeGrowth=BaseValue.RangeGrowth.new({"value":0,"max":100,"min":0,"min_growth":5,"max_growth":10,"growth_factor":8,"should_only_grow_max_value":true})
+	var _mana:BaseValue.RangeGrowth=BaseValue.RangeGrowth.new({"value":0,"max":100,"min":0,"min_growth":5,"max_growth":10,"growth_factor":8,"should_only_grow_max_value":true}):set=set_mana,get=get_mana
 	# 修仙者攻击力（使用RandomGrowth进行可变伤害计算）
-	var _attack:BaseValue.RandomGrowth=BaseValue.RandomGrowth.new({"min":8,"max":15,"min_growth":1,"max_growth":3,"growth_factor":2})
+	var _attack:BaseValue.RandomGrowth=BaseValue.RandomGrowth.new({"min":8,"max":15,"min_growth":1,"max_growth":3,"growth_factor":2}):set=set_attack,get=get_attack
 	# 修仙者防御力（使用RandomGrowth进行可变伤害减免）
-	var _defense:BaseValue.RandomGrowth=BaseValue.RandomGrowth.new({"min":5,"max":10,"min_growth":1,"max_growth":2,"growth_factor":1.5})
+	var _defense:BaseValue.RandomGrowth=BaseValue.RandomGrowth.new({"min":5,"max":10,"min_growth":1,"max_growth":2,"growth_factor":1.5}):set=set_defense,get=get_defense
 	#region 外部方法
 	## 是否能够升级
 	func can_level_up() -> bool:
