d679570b817937f580d0dcd1238f927b8d761eaf
seven.it.work <seven.it.work@gmail.com>
2025-11-17 20:58:33 +0800

    feat(装备组件): 添加背包UI引用和物品添加功能
    - 在主组件中添加背包UI常量和节点引用
    - 在demo场景中实现随机武器添加到背包的功能

--- godot-compoent/components/装备组件/demo/装备信息/测试装备.gd
+++ godot-compoent/components/装备组件/demo/装备信息/测试装备.gd
@@ -1,8 +1,11 @@
 extends Control
 const BaseItem=preload("res://entity/BaseItem.gd")
+const 装备UI=preload("uid://bogy42c77a0gw")
+const 背包UI= preload("uid://bo5yq44dywrje")
 
 
 func _on_随机添加_pressed() -> void:
 	var wepon=BaseItem.WeaponItem.random_weapon()
-	print(wepon)
+	var backpck=($"装备".背包 as 背包UI).get_backpck()
+	backpck.addItem(wepon)
 	pass # Replace with function body.
--- godot-compoent/components/装备组件/装备.gd
+++ godot-compoent/components/装备组件/装备.gd
@@ -1,1 +1,5 @@
 extends Control
+
+const 背包UI= preload("uid://bo5yq44dywrje")
+
+@onready var 背包:背包UI=$"背包"
