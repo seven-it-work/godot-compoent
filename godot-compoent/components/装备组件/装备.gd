94cf0e00387bdc7349b5d9bc8e443a1084bf6089
seven.it.work <seven.it.work@gmail.com>
2025-11-20 21:54:41 +0800

    feat(装备组件): 添加装备栏选择和筛选功能
    - 新增信号处理函数用于装备栏选择和物品筛选
    - 添加装备类型信号通知机制
    - 实现装备栏UI选中状态切换时触发筛选功能

--- godot-compoent/components/装备组件/装备.gd
+++ godot-compoent/components/装备组件/装备.gd
@@ -9,3 +9,13 @@ func _on_装备_pressed() -> void:
 	if 背包.get_selected_compartment():
 		pass
 	pass # Replace with function body.
+
+
+func _on_margin_container_选择装备栏信号(装备类型: String) -> void:
+	# todo 每当切换类型时，需要调整 $"HBoxContainer/筛选" 筛选的值
+	pass # Replace with function body.
+
+
+func _on_筛选_item_selected(index: int) -> void:
+	# todo 当筛选时，需要调整 $"背包" 中展示的物品信息，比如筛选的武器，就只展示武器
+	pass # Replace with function body.
--- godot-compoent/components/装备组件/装备组件.gd
+++ godot-compoent/components/装备组件/装备组件.gd
@@ -5,8 +5,11 @@ const BaseItemScope = preload("res://entity/BaseItem.gd")
 const BaseValue = preload("res://components/成长属性组件/基础成长.gd")
 
 @export var 是否可以点击:bool=true
-var _选中的ui:ItemScopeUI
+var _选中的ui:ItemScopeUI:get=get_选中的ui
+signal 选择装备栏信号(装备类型:String)
 
+func get_选中的ui()->ItemScopeUI:
+	return _选中的ui
 
 func _on_护盾_clicked(compartment:ItemScopeUI, item:BaseItemScope.BaseItem) -> void:
 	_on_item_click(compartment,item)
@@ -44,9 +47,13 @@ func _on_item_click(compartment:ItemScopeUI, item:BaseItemScope.BaseItem):
 	if compartment==_选中的ui:
 		_选中的ui.change_style("默认")
 		_选中的ui=null
+		选择装备栏信号.emit("全部")
 		return
 	if _选中的ui:
 		_选中的ui.change_style("默认")
 	_选中的ui=compartment
 	_选中的ui.change_style("选中")
+	# todo 这里需要做的是：信号通知，选择 xx物品。比如 item 是 BaseItemScope.WeaponItem 类型 说明 选择了武器
+	if item is BaseItemScope.WeaponItem:
+		选择装备栏信号.emit("武器")
 	pass
